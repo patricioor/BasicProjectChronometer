da46ab9060a3df646918a35f2dabc90d7153b82a
Tico <patricio.osterno1@gmail.com>
2023-09-02 10:09:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Chronometer;
+
+class Program{
+    public static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundo => 10s = 10 segundos");
+        Console.WriteLine("M = Minutos => 10m = 10 minutos");
+        Console.WriteLine("0 - Sair");
+
+        string data = Console.ReadLine().ToLower();
+        if(data == "0"){
+            System.Console.WriteLine("Finalizando programa..");
+            Environment.Exit(0);     
+        }
+        char type = char.Parse(data.Substring(data.Length - 1, 1));
+        int time = int.Parse(data[..^1]);
+        int multiplier = 1;
+
+        if(type == 'm')
+            multiplier = 60;
+        
+        Start(time * multiplier);
+    }
+
+    static void Start(int tempo){
+        var contador = 0;
+
+        while(contador < tempo){
+            Console.Clear();
+            contador++;
+            Console.WriteLine(contador);
+            Thread.Sleep(1000);
+        }
+
+        Console.Clear();
+        Console.WriteLine("Stopwatch finalizado");
+        Thread.Sleep(2000);
+        Menu();
+    }
+}
